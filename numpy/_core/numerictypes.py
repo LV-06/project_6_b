344489b5394b8ff341f73dda6bae51822e3b59e2
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2024-07-05 23:31:52 +0200

    MAINT: apply ruff/Pycodestyle rule F401
    F401 imported but unused
    
    I have left out *.pyi files. Still lots of occurrences to examine.

--- numpy/_core/numerictypes.py
+++ numpy/_core/numerictypes.py
@@ -81,7 +81,7 @@ import warnings
 
 from . import multiarray as ma
 from .multiarray import (
-        ndarray, array, dtype, datetime_data, datetime_as_string,
+        ndarray, dtype, datetime_data, datetime_as_string,
         busday_offset, busday_count, is_busday, busdaycalendar
         )
 from .._utils import set_module
@@ -95,9 +95,6 @@ __all__ = [
 
 # we don't need all these imports, but we need to keep them for compatibility
 # for users using np._core.numerictypes.UPPER_TABLE
-from ._string_helpers import (
-    english_lower, english_upper, english_capitalize, LOWER_TABLE, UPPER_TABLE
-)
 
 from ._type_aliases import (
     sctypeDict, allTypes, sctypes
@@ -106,7 +103,7 @@ from ._dtype import _kind_name
 
 # we don't export these for import *, but we do want them accessible
 # as numerictypes.bool, etc.
-from builtins import bool, int, float, complex, object, str, bytes
+from builtins import bool, int, float, complex, str, bytes
 
 
 # We use this later
--- numpy/testing/overrides.py
+++ numpy/testing/overrides.py
@@ -63,7 +63,6 @@ def get_overridable_numpy_array_functions():
     """
     # 'import numpy' doesn't import recfunctions, so make sure it's imported
     # so ufuncs defined there show up in the ufunc listing
-    from numpy.lib import recfunctions
     return _array_functions.copy()
 
 def allows_array_function_override(func):
